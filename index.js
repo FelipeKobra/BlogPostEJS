a90a4170bb94e29d84be9a4bcdf6ef152a9c0035
FelipeKobra <felipekbra@gmail.com>
2023-12-20 13:53:59 -0300

    Mudei Lógica e Estilização
    1 - Troquei a paleta de cores
    2 - Adicionei responsividade
    3- Retirei as tags por falta de necessidade
    4 - Mudei a funcionalidade do Editar para ficar um código mais limp
    5 - Adicionei uma rota para cada post

--- index.js
+++ index.js
@@ -10,17 +10,15 @@ const app = express();
 const port = 3000;
 
 app.locals.__dirname = __dirname;
-app.locals.postList = postList
+app.locals.postList = postList;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
-function cards (titulo, subtitulo, post, tags, data){
+function cards(titulo, subtitulo, post, data) {
   this.titulo = titulo;
   this.subtitulo = subtitulo;
   this.post = post;
-  this.tags = tags;
   this.data = data;
-
 }
 
 app.get("/", (req, res) => {
@@ -33,51 +31,62 @@ app.get("/create", (req, res) => {
 
 app.post("/create", (req, res) => {
   const currentDate = format(new Date(), "dd/MM/yyyy");
-  const card = new cards (req.body.titulo,req.body.subtitulo,req.body.post,req.body.tags,currentDate)
-  postList.push(card)
+  const card = new cards(
+    req.body.titulo,
+    req.body.subtitulo,
+    req.body.post,
+    currentDate
+  );
+  postList.push(card);
   res.redirect("/");
+});
+
+app.get("/post/:id", (req, res) => {
+  const index = req.params.id;
+  const post = postList[index];
+  res.render(__dirname + "/views/post.ejs", {
+    index: index,
+    titulo: post.titulo,
+    subtitulo: post.subtitulo,
+    post: post.post,
+  });
 });
 
 app.post("/delete", (req, res) => {
-  const indexToRemove = parseInt(req.body.delete.match(/(\d+)/)[1], 10);
-  
-  if (!isNaN(indexToRemove) && indexToRemove >= 0 && indexToRemove < postList.length) {
-    postList.splice(indexToRemove, 1);
-  }
+  const index = req.body.index;
+  postList.splice(index, 1);
   res.redirect("/");
 });
-
 
 app.post("/edit", (req, res) => {
-  const selectedIndex = parseInt(req.body.edit, 10);
-  const selectedPost = postList[selectedIndex];
-    res.render(__dirname + "/views/edit.ejs", {selectedPost,selectedIndex});
-  }
-);
-
-app.post("/editpos", (req, res) => {
-  const indexToUpdate = parseInt(req.body.index, 10);
-  const updatedPost = {
-      titulo: req.body.titulo,
-      subtitulo: req.body.subtitulo,
-      post: req.body.post,
-      tags: req.body.tags
-  };
-
-  // Certifique-se de que o índice é válido antes de atualizar
-  if (!isNaN(indexToUpdate) && indexToUpdate >= 0 && indexToUpdate < postList.length) {
-      postList[indexToUpdate] = updatedPost;
-      console.log("Post atualizado:", updatedPost);
-  } else {
-      console.log("Índice inválido durante a atualização.");
-  }
-
-  res.redirect("/");
+  const index = req.body.index;
+  const post = postList[index];
+  res.render(__dirname + "/views/edit.ejs", {
+    index: index,
+    titulo: post.titulo,
+    subtitulo: post.subtitulo,
+    post: post.post,
+  });
 });
 
-
-
+app.post("/editpos", (req, res) => {
+  const currentDate = format(new Date(), "dd/MM/yyyy");
+  const index = req.body.index;
+  const titulo = req.body.titulo;
+  const subtitulo = req.body.subtitulo;
+  const postWrite = req.body.post;
+  postList[index] = new cards(
+    titulo,
+    subtitulo,
+    postWrite,
+    currentDate,
+  );
+  res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log("Running on " + port);
 });
+
+
+
